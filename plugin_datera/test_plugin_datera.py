41ea43e06f95eb2021ed58ec9cc97ae5b7979af8
Funs Kessen <funs@barred.org>
2016-06-22 18:07:24 +0200

    added the mirror and update core repos test case

--- plugin_datera/test_plugin_datera.py
+++ plugin_datera/test_plugin_datera.py
@@ -34,6 +34,15 @@ class DateraPlugin(TestBasic):
     """DateraPlugin, a plugin to test datera integration with Fuel"""
 
     @classmethod
+    def am_mos_version(cls, version):
+        """ Check the MOS version we are """
+        if CONF.RELEASE_VERSION == "2015.1.0-7.0" and version == '7.0':
+            return True
+        if CONF.RELEASE_VERSION == "2015.1.0-8.0" and version == '8.0':
+            return True
+        return False
+
+    @classmethod
     def check_datera_cinder_config(cls, remote, path):
         """ checks the cinder config for datera """
         command = 'cat {0}'.format(path)
@@ -42,23 +51,24 @@ class DateraPlugin(TestBasic):
         cinder_conf = ConfigParser.ConfigParser()
         cinder_conf.readfp(conf_data)
 
-        # now that is odd!!!! no cinder?
-        asserts.assert_equal(
-            cinder_conf.get('DEFAULT', 'volume_driver'),
-            'cinder.volume.drivers.datera.DateraDriver')
-        asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_ip'))
-        asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_login'))
-        asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_password'))
-        asserts.assert_true(cinder_conf.has_option('DEFAULT',
-                                                   'datera_num_replicas'))
+        if cinder_conf.has_option('DEFAULT', 'enabled_backends'):
+            print "multiple backends"
+        else:
+            asserts.assert_equal(
+                cinder_conf.get('DEFAULT', 'volume_driver'),
+                'cinder.volume.drivers.datera.DateraDriver')
+
+            asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_ip'))
+            asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_login'))
+            asserts.assert_true(cinder_conf.has_option('DEFAULT', 'san_password'))
+            asserts.assert_true(cinder_conf.has_option('DEFAULT',
+                                                       'datera_num_replicas'))
 
     @classmethod
-    def check_service(cls, remote, service):
+    def get_pids(cls, remote, proc):
         """ Check if a service exists on a remote node """
-        ps_output = ''.join(
-            remote.execute('ps ax | grep {0} | '
-                           'grep -v grep'.format(service))['stdout'])
-        return service in ps_output
+        ps_output = [x.rstrip('\n') for x in remote.execute('pgrep {0}'.format(proc))['stdout']]
+        return ps_output
 
     @classmethod
     def fetch_plugin_version(cls, remote, pluginname):
@@ -93,15 +103,13 @@ class DateraPlugin(TestBasic):
         options = ["datera_mvip", "datera_admin_login",
                    "datera_admin_password", "datera_num_replicas"]
         fpattr = attr["editable"]["fuel-plugin-datera-cinder"]
-        # check plugin installed and attributes have datera options
-        if CONF.RELEASE_VERSION == "2015.1.0-7.0":
+        if self.am_mos_version("7.0"):
             for option in options:
                 asserts.assert_true(option in fpattr,
                                     "{0} is not in cluster attributes: {1}".
                                     format(option, str(attr))
                                    )
-        elif CONF.RELEASE_VERSION == "2015.1.0-8.0":
-            # in 8.0 this bevame a list..
+        elif self.am_mos_version("8.0"):
             for nattr in fpattr["metadata"]["versions"]:
                 for option in options:
                     asserts.assert_true(option in nattr,
@@ -117,12 +125,12 @@ class DateraPlugin(TestBasic):
         # enable Datera plugin
         datopts = attr["editable"]["fuel-plugin-datera-cinder"]
         datopts["metadata"]["enabled"] = True
-        if CONF.RELEASE_VERSION == "2015.1.0-7.0":
+        if self.am_mos_version("7.0"):
             datopts["datera_mvip"]["value"] = CONF.DATERA_MVIP
             datopts["datera_admin_login"]["value"] = CONF.DATERA_USERNAME
             datopts["datera_admin_password"]["value"] = CONF.DATERA_PASSWORD
             datopts["datera_num_replicas"]["value"] = CONF.DATERA_NUM_REPLICAS
-        else:
+        elif self.am_mos_version("8.0"):
             for opt in datopts["metadata"]["versions"]:
                 opt["datera_mvip"]["value"] = CONF.DATERA_MVIP
                 opt["datera_admin_login"]["value"] = CONF.DATERA_USERNAME
@@ -130,6 +138,27 @@ class DateraPlugin(TestBasic):
                 opt["datera_num_replicas"]["value"] = CONF.DATERA_NUM_REPLICAS
             attr["editable"]["fuel-plugin-datera-cinder"] = datopts
         self.fuel_web.client.update_cluster_attributes(cluster_id, attr)
+
+    @classmethod
+    def deploy_verify_ostf(cls, self, cid, timer, check, can_fail):
+        """ Deploy, verify and run OSTF """
+        self.fuel_web.deploy_cluster_wait(cid, timeout=timer, check_services=check)
+        self.fuel_web.verify_network(cid)
+        # FEATURE: Fix the function when "feature" #1457515 will be fixed.
+        if can_fail:
+            self.fuel_web.run_ostf(
+                cluster_id=cid, test_sets=['smoke', 'sanity', 'ha'],
+                should_fail=1,
+                failed_test_name=['Check that required services are running'])
+        else:
+            self.fuel_web.run_ostf(cluster_id=cid)
+
+    @classmethod
+    def count_fuel_nodes_state(cls, node, status="ready"):
+        """ Check with fuel how many nodes are in a given state """
+        cmd = "fuel nodes | grep %s | wc -l" % (status)
+        node_count = int(''.join(node.execute("%s" % (cmd))['stdout']).rstrip('\n'))
+        return node_count
 
     # should be a different setup perhaps...
     @test(depends_on=[SetupEnvironment.prepare_slaves_5],
@@ -156,18 +185,16 @@ class DateraPlugin(TestBasic):
             self.env.revert_snapshot("datera_plugin_deploy_env")
             return
 
-        # copy plugin to the master node
         checkers.upload_tarball(
             self.env.d_env.get_admin_remote(),
             CONF.DATERA_PLUGIN_PATH, '/var')
 
-        # install plugin
         checkers.install_plugin_check_code(
             self.env.d_env.get_admin_remote(),
             plugin=os.path.basename(CONF.DATERA_PLUGIN_PATH))
 
         settings = None
-        if CONF.RELEASE_VERSION == "2015.1.0-7.0" and CONF.NEUTRON_ENABLE:
+        if self.am_mos_version("7.0") and CONF.NEUTRON_ENABLE:
             settings = {
                 "net_provider": 'neutron',
                 "net_segment_type": CONF.NEUTRON_SEGMENT_TYPE
@@ -193,18 +220,12 @@ class DateraPlugin(TestBasic):
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        # get remotes for all nodes
+        nodes = ['slave-01', 'slave-02', 'slave-03']
         controller_nodes = [self.fuel_web.get_nailgun_node_by_name(node)
-                            for node in ['slave-01', 'slave-02', 'slave-03']]
-        # compute_nodes = [self.fuel_web.get_nailgun_node_by_name(node)
-        #                    for node in ['slave-04', 'slave-05']]
-
+                            for node in nodes]
         controller_remotes = [self.env.d_env.get_ssh_to_remote(node['ip'])
                               for node in controller_nodes]
-        # compute_remotes = [self.env.d_env.get_ssh_to_remote(node['ip'])
-        #                   for node in compute_nodes]
 
-        # check cinder-volume settings
         for remote in controller_remotes:
             self.check_datera_cinder_config(
                 remote=remote, path='/etc/cinder/cinder.conf')
@@ -242,32 +263,91 @@ class DateraPlugin(TestBasic):
         self.env.revert_snapshot("datera_plugin_deploy_env")
         cluster_id = self.fuel_web.get_last_created_cluster()
 
-        # remove a controller
         self.fuel_web.update_nodes(
             cluster_id, {'slave-03': ['controller', 'cinder']}, False, True
         )
-        self.fuel_web.deploy_cluster_wait(cluster_id, timeout=11700, check_services=False)
-        self.fuel_web.verify_network(cluster_id)
         # FEATURE: Fix the function when "feature" #1457515 will be fixed.
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id, test_sets=['smoke', 'sanity', 'ha'],
-            should_fail=1,
-            failed_test_name=['Check that required services are running'])
-        # datera API validation ?
+        self.deploy_verify_ostf(self, cluster_id, 11700, False, True)
 
-        # add it back again
         self.fuel_web.update_nodes(
             cluster_id, {'slave-03': ['controller', 'cinder']}
         )
-        self.fuel_web.deploy_cluster_wait(cluster_id, timeout=11700, check_services=False)
-        self.fuel_web.verify_network(cluster_id)
         # FEATURE: Fix the function when "feature" #1457515 will be fixed.
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id, test_sets=['smoke', 'sanity', 'ha'],
-            should_fail=1,
-            failed_test_name=['Check that required services are running'])
+        self.deploy_verify_ostf(self, cluster_id, 11700, False, True)
 
-        # self.env.make_snapshot("datera_plugin_modify_env", is_make=True)
+    @test(depends_on=[datera_plugin_deploy_env],
+          groups=["datera_f_c_m_update_core_repos"])
+    @log_snapshot_after_test
+    def datera_f_c_m_update_core_repos(self):
+        """Deploy cluster with datera plugin and modify master
+
+        1. Copy the plugin to the Fuel Master node (please refer to the User
+           Guide for more details).
+        2. Install the plugin.
+        3. Ensure that the plugin is installed successfully using CLI with
+           running fuel plugins --list command in the Fuel CLI.
+        4. Create an environment with enabled plugin in the Fuel Web UI.
+        5. Add 3 nodes with Controller role and 1 node with Compute and
+           another role.
+        6. Finalize environment configuration (e.g. networking, nodes interfaces).
+        7. Enable the plugin and configure it following the instructions from
+           the Plugin Guide.
+        8. Run network verification check.
+        9. Deploy the cluster.
+        10. Run OSTF.
+        11. Go in cli through controller / compute / storage /etc nodes and
+            get pid of services which were launched by plugin and store them.
+        12. Launch the following command on the Fuel Master node:
+            fuel-createmirror -M
+        13. Launch the following command on the Fuel Master node:
+            a. For MOS < 8.0:
+            fuel --env <ENV_ID> node --node-id <NODE_ID1> <NODE_ID2> <NODE_ID_N>
+               --tasks upload_core_repos
+            b. For MOS 8.0:
+            fuel --env <ENV_ID> node --node-id <NODE_ID1> <NODE_ID2> <NODE_ID_N>
+                --tasks setup_repositories
+        14. Go to controller/plugin/storage node and check if plugin's services
+            are alive and aren't changed their pid.
+        15. Check with fuel nodes command that all nodes are remain in ready
+            status.
+        16. Rerun OSTF.
+
+        Duration 1.5h
+        Snapshot datera_fuel_create_mirror_update_core_repos
+        """
+        self.env.revert_snapshot("datera_plugin_deploy_env")
+        cluster_id = self.fuel_web.get_last_created_cluster()
+        nodes = ['slave-01', 'slave-02', 'slave-03']
+        controller_nodes = [self.fuel_web.get_nailgun_node_by_name(node)
+                            for node in nodes]
+
+        ppids = {}
+        for node in controller_nodes:
+            remote = self.env.d_env.get_ssh_to_remote(node['ip'])
+            ppids[node['ip']] = self.get_pids(remote, "cinder-volume")
+
+        self.env.d_env.get_admin_remote().execute("fuel-createmirror -M")
+        tasks = "setup_repositories"
+        if self.am_mos_version("7.0"):
+            tasks = "upload_core_repos"
+        cmd = "fuel --env %s node --node-id %s --tasks %s" % (cluster_id,
+                                                              ' '.join(nodes),
+                                                              tasks)
+        self.env.d_env.get_admin_remote().execute(cmd)
+
+        for node in controller_nodes:
+            remote = self.env.d_env.get_ssh_to_remote(node['ip'])
+            pids = self.get_pids(remote, "cinder-volume")
+            asserts.assert_equal(ppids[node['ip']], pids)
+
+        node_count = len(self.env.d_env.get_nodes()) - 1
+        node_real_count = self.count_fuel_nodes_state(
+            node=self.env.d_env.get_admin_remote(),
+            status="ready")
+        msg = "Real count %s did not match count %s" % (node_real_count,
+            node_count)
+        asserts.assert_equal(node_count, node_real_count, msg)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
 
     @test(depends_on=[datera_plugin_deploy_env],
           groups=["datera_plugin_modify_env_1"])
@@ -299,25 +379,12 @@ class DateraPlugin(TestBasic):
         self.fuel_web.update_nodes(
             cluster_id, {'slave-04': ['compute']}, False, True
         )
-        # one service will be missing.....
-        self.fuel_web.deploy_cluster_wait(cluster_id, check_services=False)
-        self.fuel_web.verify_network(cluster_id)
         # FEATURE: Fix the function when "feature" #1457515 will be fixed.
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id, test_sets=['smoke', 'sanity', 'ha'],
-            should_fail=1,
-            failed_test_name=['Check that required services are running'])
+        self.deploy_verify_ostf(self, cluster_id, 11700, False, True)
 
         self.fuel_web.update_nodes(cluster_id, {'slave-04': ['compute']})
-        self.fuel_web.deploy_cluster_wait(cluster_id, check_services=False)
-        self.fuel_web.verify_network(cluster_id)
         # FEATURE: Fix the function when "feature" #1457515 will be fixed.
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id, test_sets=['smoke', 'sanity', 'ha'],
-            should_fail=1,
-            failed_test_name=['Check that required services are running'])
-        # datera API validation ?
-        # self.env.make_snapshot("datera_plugin_modify_env_1", is_make=True)
+        self.deploy_verify_ostf(self, cluster_id, 11700, False, True)
 
     @test(depends_on=[datera_plugin_deploy_env],
           groups=["datera_plugin_deployed_remove"])
@@ -349,7 +416,6 @@ class DateraPlugin(TestBasic):
             self.env.d_env.get_admin_remote(),
             plugin="fuel-plugin-datera-cinder==%s" % version,
             exit_code=1)
-        # self.env.make_snapshot("datera_plugin_deployed_remove", is_make=True)
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_5],
           groups=["datera_plugin_remove"])
@@ -389,20 +455,17 @@ class DateraPlugin(TestBasic):
         )
 
         settings = None
-        if CONF.RELEASE_VERSION == "2015.1.0-7.0" and CONF.NEUTRON_ENABLE:
+        if self.am_mos_version("7.0") and CONF.NEUTRON_ENABLE:
             settings = {
                 "net_provider": 'neutron',
                 "net_segment_type": CONF.NEUTRON_SEGMENT_TYPE
             }
-
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
             mode=CONF.DEPLOYMENT_MODE,
             settings=settings
         )
-
         self.set_datera_attributes(self, cluster_id)
-
         self.fuel_web.update_nodes(
             cluster_id,
             {
@@ -414,7 +477,6 @@ class DateraPlugin(TestBasic):
             }
         )
         self.fuel_web.verify_network(cluster_id)
-
         self.fuel_web.client.delete_cluster(cluster_id)
         # should return 0 as it's not used
         self.remove_plugin_check(
@@ -422,3 +484,10 @@ class DateraPlugin(TestBasic):
             plugin="fuel-plugin-datera-cinder==%s" % version,
             exit_code=0)
         # self.env.make_snapshot("datera_plugin_remove", is_make=True)
+
+    # @test(depends_on=[datera_plugin_deploy_env],
+    #       groups=["resiliency_tests"])
+    # @log_snapshot_after_test
+    # def datera_resiliency_tests(self):
+    #     self.env.revert_snapshot("datera_plugin_deploy_env")
+    #     cluster_id = self.fuel_web.get_last_created_cluster()
